487ff639302416bd5c425bf7672aa008b353709e
Victor <victormnuzzi003@gmail.com>
2023-05-21 11:56:19 -0300

    CSS arrumado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('header nav ul li a');
+    var links = document.querySelectorAll('a');
     for (var i = 0; i < links.length; i++) {
         links[i].addEventListener('click', function(e) {
             e.preventDefault();
-            
             var targetSection = document.querySelector(this.getAttribute('href'));
             window.scrollTo({
                 top: targetSection.offsetTop,
